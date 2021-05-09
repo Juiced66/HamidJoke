707cb565f7444a534a07a1b954f8cdfced3d38df
Jamesetco66 <80784430+Juiced66@users.noreply.github.com>
2021-05-09 12:26:51 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+Générateur de blagues demandé a de nombreuses reprises par l'ami Hammid . 
+
+Utilisation de React pour le rendu.
+
+Appel a l'API https://www.blagues-api.fr/ .
+
+TODO : Blagues par catégorie, CSS pour faire parler Hammid
+
+En cas de réutilisation : 
+
+npm i
+
+modifier le TOKEN de l'API (disponnible sur leur site) 
+
+npm start 
+
---
+++ src/App.js
@@ -0,0 +1,45 @@
+import { useEffect, useState } from 'react';
+import img from './hammid.png'
+
+const API_URL = 'https://www.blagues-api.fr/api/random'
+const API_TOKEN = {
+  headers: {
+      'Authorization': `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiMjcxOTk4OTY5ODYzNzMzMjQ4IiwibGltaXQiOjEwMCwia2V5IjoidjVvTkJvTmgxSnFVd1ZUeU9Mb0VLaFhPM3NZS3g2WktRTWxDQlhlekdvRTRzd1p1ZlAiLCJjcmVhdGVkX2F0IjoiMjAyMS0wNS0wOVQwOTozNjo1OSswMDowMCIsImlhdCI6MTYyMDU1MzAxOX0.uk0XGCW4z5pR6ltFZUPiIdhu22GpAVw6gu3_fQasJ-Q`  
+  }
+}
+
+
+function App() {
+
+  const [joke, setJoke] = useState('');
+  const [answer, setAnswer] = useState('');
+
+  const generateJoke = () => {
+    fetch(API_URL, API_TOKEN )
+  .then(res => res.json())
+  .then(data => {
+    setJoke(data.joke);
+    setAnswer(data.answer)
+  }) 
+}  
+
+  useEffect(()=> {
+    generateJoke();
+  }, []);
+
+  return (
+    <div className = "container">
+      <div className = "box">
+      <h1>Hammid Joke Generator</h1>
+      <p>{joke}</p>
+      <p>{answer}</p>
+      <button onClick ={generateJoke}> Une autre ! 😀</button>
+      </div>
+      <img src={img} alt="mon image" />
+    </div>
+    
+  );
+
+}
+
+export default App
